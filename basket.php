b85c498094b6e2b3cb1124fd6ccf34d11414a29b
AhamedMuaaz <mma.muaaz@gmail.com>
2019-04-06 18:11:59 +0530

    added php verification for seting a session variable

--- basket.php
+++ basket.php
@@ -9,16 +9,18 @@ echo "<body>";
 include ("headfile.html"); //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 
+
+if(isset($_POST['h_prodid'])){
 $newprodid=$_POST['h_prodid'];
 $reququantity=$_POST['quantity'];
 $_SESSION['basket'][$newprodid]=$reququantity;
 
-echo "ID of selectedProduct is ".$newprodid."<br>";
-echo "Quantity Of Selected Product ".$reququantity."<br>";
-
-echo "<p>The doc id ".$newdocid." has been ".$_SESSION['basket'][$newdocid];
-echo "<p>1 item added";
-
+//echo "ID of selectedProduct is ".$newprodid."<br>";
+//echo "Quantity Of Selected Product ".$reququantity."<br>";
+echo "<p>1 item added";}
+else{
+	echo "Current basket unchanged";
+}
 
 include("footfile.html"); //include head layout
 echo "</body>";
